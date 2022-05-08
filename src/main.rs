e08eb5e4a8affb5960e935843ddd802257a770dc
monsieurwinner <komisiabi.42@gmail.com>
2022-05-08 23:18:17 +0100

    Adding flush

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, ptr::hash};
+use std::collections::HashMap;
 fn main() {
    let mut argu = std::env::args().skip(1);
    let key = argu.next().unwrap();
@@ -6,10 +6,14 @@ fn main() {
 
    println!("The key is {} and the value is {}", key,value);
    // Let create a file and save in our project dir using the std:fs library
-   let contents = format!("{}\t{}\n",key,value);
-   std::fs::write("kv.db" , contents);
+   //let contents = format!("{}\t{}\n",key,value);
+   //std::fs::write("kv.db" , contents);
 
-   let databade = Database::new().expect("Creating DB failed");
+   let mut database = Database::new().expect("Creating DB failed");
+   //database.insert(key, value);
+   database.insert(key.to_uppercase(), value.clone()); 
+   database.insert(key, value);
+   database.flush().unwrap();
 }
 
 struct Database{
@@ -20,6 +24,7 @@ struct Database{
 impl Database {
 
     //Here we can define our functions with fn
+    //The new function is to create a record
     fn new() -> Result<Database, std::io::Error> {
         let mut map = HashMap::new();
 
@@ -31,11 +36,30 @@ impl Database {
             let key = chunks.next().expect("No key!");
             let value = chunks.next().expect("No value!");
 
-            map.insert(key, value);
+            map.insert(key.to_owned(), value.to_owned());
         }
-        Ok(Database{
-            map : HashMap::new(),
-        })
+        Ok(Database{ map : map })
+    }
+
+    // Insert record into our KvStore database iwith a method
+    // Mehtods are similar to function and declared using fn. But the 1st parameter of a method is always "self"
+    fn insert(&mut self, key: String, value: String) {
+        self.map.insert(key,value); 
+    }
+
+    //Flush method
+    fn flush(self)-> std::io::Result<()> {
+        let mut contents = String::new();
+        for (key, value) in &self.map {
+            contents.push_str(key);
+            contents.push('\t');
+            contents.push_str(value);
+            contents.push('\n');
+            //let kvpairs = format!("{}\t{}\n", key, value);
+            //contents.push_str(&kvpairs);
+        }
+        std::fs::write("kv.db", contents)
+        //todo!()
     }
 }
 
